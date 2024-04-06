b207659dd57283ac487081b2aeaa98af4a713352
Samuel <samuelrivba@gmail.com>
2024-04-05 20:38:26 -0500

    Add best implementations for stacks an queues

---
+++ stacksqueues.js
@@ -0,0 +1,15 @@
+// Stacks
+
+// google
+// udemy
+// youtube
+
+// Arrays for performance
+// Linked Lists for the dynamic memory
+
+// Queues
+
+// Matt - Joy -- Samir -- Pavel
+
+// Arrays have indexes, O(n) to unshift
+// Linked Lists work better with the head pointers
